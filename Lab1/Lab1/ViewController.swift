4c97ca38b87a993731ef418e46ecd29119be9ad5
saharapanna <43882742+saharapanna@users.noreply.github.com>
2020-01-29 11:47:46 -0500

    unfinished lab-1

---
+++ Lab1/Lab1/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Tech on 2020-01-29.
+//  Copyright © 2020 GBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var IBO_HeightTextBox: UITextField!
+    
+    @IBOutlet weak var IBO_WeightTextBox: UITextField!
+
+
+
+    @IBAction func IBO_CalculatorButton(_ sender: Any) {
+        // this is an action
+        print("The button has been clicked!");
+
+    }
+    
+
+
+
+
+}
+
