f54a211ad5fe097a7c80abbd3d929ef0361c426f
saharapanna <43882742+saharapanna@users.noreply.github.com>
2020-03-11 11:52:01 -0400

    image view not finished

---
+++ march11lab/march11lab/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  march11lab
+//
+//  Created by Tech on 2020-03-11.
+//  Copyright © 2020 Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // label outlets
+    
+    @IBOutlet weak var switchStatus: UILabel!
+    
+    
+    @IBOutlet weak var tepperLabel: UILabel!
+    
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    
+    // stepper status
+    
+    
+    
+    
+    
+    // Slider status
+    
+    
+    
+    
+    
+    
+
+
+}
+
